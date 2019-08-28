97585d25cb734db1fc751bad7bbc42cd1731619c
pinkipi <yarnovax@gmail.com>
2019-08-27 21:30:06 -0400

    Rewrite for patch 85

--- index.js
+++ index.js
@@ -1,129 +1,53 @@
 const BAMARAMA_BOX = 80086,
 	ROOT_BEER = 80081,
 	TRASH = {
-		80078: 'beer',
-		80079: 'wine',
-		80080: 'lotus',
-		80082: 'moongourd',
-		80089: 'afro',
-		80090: 'chefHat'
+		beer:		80078,
+		wine:		80079,
+		moongourd:	80082,
+		afro:		80089,
+		chefHat:	80090
 	},
-	HATS = [80089, 80090], // Afro, Chef's
-	ITEMS = [ROOT_BEER, ...Object.keys(TRASH).map(id => Number(id))]
+	HATS = [TRASH.afro, TRASH.chefHat],
+	ITEMS = [ROOT_BEER, ...Object.values(TRASH)]
 
 module.exports = function RootBeer(mod) {
-	const SETTINGS_VERSION = 1
-	if(mod.settings._version !== SETTINGS_VERSION)
-		mod.settings = {
-			_version: SETTINGS_VERSION,
+	mod.settings.$init({
+		version: 2,
+		defaults: {
 			autoTrash: true,
 			autoTrashItems: {
 				beer: true,
 				wine: true,
-				lotus: true,
 				moongourd: true,
 				afro: true,
 				chefHat: true
 			}
 		}
-
-	const {command} = mod.require
-
-	let hooks = [],
-		gameId = -1n,
-		enabled = false,
-		timer = null,
-		myLocation = null,
-		statTotal = 0,
-		statRootBeers = 0,
-		invenHook = null
-
-	command.add('rootbeer', () => {
-		if(enabled = !enabled) {
-			load()
-			openBox()
-			command.message('Auto-Rootbeer started.')
-		}
-		else stop()
 	})
 
-	mod.hook('S_LOGIN', mod.patchVersion < 81 ? 12 : 13, event => { ({gameId} = event) })
-	mod.hook('S_SPAWN_ME', 3, event => { myLocation = event })
-	mod.hook('C_PLAYER_LOCATION', 5, event => { myLocation = event })
+	const {command, game} = mod.require,
+		{inventory} = game
 
-	function openBox() {
-		mod.send('C_USE_ITEM', 3, {
-			gameId,
-			id: BAMARAMA_BOX,
-			amount: 1,
-			loc: myLocation.loc,
-			w: myLocation.w,
-			unk4: true
-		})
+	let hooks = [],
+		enabled = false,
+		timer = null,
+		statTotal = 0,
+		statRootBeers = 0
 
-		timer = mod.setTimeout(openBox, 5000) // Fallback in case a box failed to open
-	}
+	command.add('rootbeer', () => { (!enabled ? start : stop)() })
 
-	function stop() {
-		mod.clearTimeout(timer)
-		unload()
-		enabled = false
-		command.message('Auto-Rootbeer stopped.' + (!statTotal ? '' : ` Unboxed ${statRootBeers}/${statTotal} (${(Math.floor(statRootBeers / statTotal * 1000) / 10) || '0'}%).`))
-		statTotal = statRootBeers = 0
-	}
+	function start() {
+		if(enabled) return
 
-	function load() {
-		function hook() { hooks.push(mod.hook(...arguments)) }
+		if(!inventory.has(BAMARAMA_BOX)) {
+			command.message('No bamarama boxes found.')
+			return
+		}
 
-		let invenItems = null
+		enabled = true
 
-		if(invenHook) mod.unhook(invenHook)
-
-		invenHook = mod.hook('S_INVEN', 18, event => {
-			invenItems = event.first ? event.items : invenItems.concat(event.items)
-
-			if(!event.more) {
-				let used = 0
-
-				if(mod.settings.autoTrash)
-					for(let item of invenItems)
-						if(item.slot >= 40)
-							used++
-
-				const strictTrash = event.size - used < 2,
-					hats = []
-
-				let box = false, idx = -1
-
-				for(let i in HATS) hats.push([])
-
-				for(let item of invenItems) {
-					if(item.slot < 40) continue // First 40 slots are reserved for equipment, etc.
-
-					if(item.id == BAMARAMA_BOX) box = true
-					else if(mod.settings.autoTrash) {
-						for(let id in TRASH)
-							if(item.id === Number(id)) {
-								// Trashing large stacks of items is more bandwidth efficient
-								if(mod.settings.autoTrashItems[TRASH[id]] && (strictTrash || HATS.includes(item.id) || item.amount >= 99 || !enabled))
-									deleteItem(item.slot, item.amount)
-
-								break
-							}
-					}
-					else if((idx = HATS.indexOf(item.id)) !== -1) hats[idx].push(item.slot)
-				}
-
-				for(let hat of hats)
-					while(hat.length >= 2) mergeItem(hat.pop(), hat[0])
-
-				if(!box) stop()
-
-				invenItems = null
-
-				if(!enabled) mod.unhook(invenHook) // Unhook after we've cleaned up
-			}
-		})
+		// Toggle hook in case we're still cleaning up from previous run
+		inventory.off('update', inventoryUpdate).on('update', inventoryUpdate)
 
 		hook('S_SYSTEM_MESSAGE_LOOT_ITEM', 1, event => {
 			if(ITEMS.includes(event.item)) {
@@ -137,6 +61,49 @@ module.exports = function RootBeer(mod) {
 		})
 
 		hook('C_RETURN_TO_LOBBY', 'raw', () => false) // Prevents you from being automatically logged out while AFK
+
+		openBox()
+		command.message('Auto-Rootbeer started.')
+	}
+
+	function hook() { hooks.push(mod.hook(...arguments)) }
+
+	function openBox() {
+		inventory.use(BAMARAMA_BOX)
+		timer = mod.setTimeout(openBox, 5000) // Fallback in case a box failed to open
+	}
+
+	function inventoryUpdate() {
+		if(mod.settings.autoTrash) {
+			const strictTrash = inventory.size - inventory.items.length < 2 || !enabled
+
+			for(let [name, id] of Object.entries(TRASH))
+				if(mod.settings.autoTrashItems[name])
+					for(let item of inventory.findAll(id))
+						// Trashing large stacks of items is more bandwidth efficient
+						if(item.amount >= 99 || HATS.includes(id) || strictTrash)
+							inventory.delete(item)
+		}
+
+		for(let hat of HATS.map(id => inventory.findAll(id)))
+			while(hat.length >= 2) inventory.merge(hat.pop(), hat[0])
+
+		if(!enabled) inventory.off('update', inventoryUpdate) // Unhook after we've cleaned up
+		else if(!inventory.has(BAMARAMA_BOX)) stop()
+	}
+
+	function stop() {
+		if(!enabled) return
+
+		enabled = false
+		mod.clearTimeout(timer)
+		timer = null
+		unload()
+
+		command.message('Auto-Rootbeer stopped.'
+			+ (!statTotal ? '' : ` Unboxed ${statRootBeers}/${statTotal} (${(Math.floor(statRootBeers / statTotal * 1000) / 10) || '0'}%).`))
+
+		statTotal = statRootBeers = 0
 	}
 
 	function unload() {
@@ -146,14 +113,4 @@ module.exports = function RootBeer(mod) {
 			hooks = []
 		}
 	}
-
-	function deleteItem(slot, amount) {
-		mod.send('C_DEL_ITEM', 2, {
-			gameId,
-			slot: slot - 40,
-			amount
-		})
-	}
-
-	function mergeItem(slotFrom, slotTo) { mod.send('C_MERGE_ITEM', 1, {slotFrom, slotTo}) }
 }
