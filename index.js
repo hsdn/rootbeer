2551a6d0b981e77c2b7e967daf754f0c3b3566d2
Dmitry S <dmitry.s@hsdn.org>
2022-04-30 23:08:01 +0400

    v92, Eslint update

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const BAMARAMA_BOX = 80086,
 	ROOT_BEER = 80081,
 	TRASH = {
-		"beer":			80078,
-		"wine":			80079,
-		"moongourd":	80082,
-		"afro":			80089,
-		"chefHat":		80090
+		"beer": 80078,
+		"wine": 80079,
+		"moongourd": 80082,
+		"afro": 80089,
+		"chefHat": 80090
 	},
 	HATS = [TRASH.afro, TRASH.chefHat],
 	ITEMS = [ROOT_BEER, ...Object.values(TRASH)];
 
 module.exports = function RootBeer(mod) {
-
-	mod.game.initialize('inventory');
+	mod.game.initialize("inventory");
 
 	let hooks = [],
 		enabled = false,
@@ -21,51 +20,49 @@ module.exports = function RootBeer(mod) {
 		statTotal = 0,
 		statRootBeers = 0;
 
-	mod.command.add('rootbeer', () => { (!enabled ? start : stop)() });
+	mod.command.add("rootbeer", () => { (!enabled ? start : stop)(); });
 
-	hook('S_SPAWN_ME', 3, event => { myLoc = event; });
-	hook('C_PLAYER_LOCATION', 5, event => { myLoc = event; });
+	hook("S_SPAWN_ME", 3, event => { myLoc = event; });
+	hook("C_PLAYER_LOCATION", 5, event => { myLoc = event; });
 
 	function start() {
 		if (enabled) return;
 
-		if(mod.game.inventory.findAll(BAMARAMA_BOX).length === 0) {
-			mod.command.message('You do not have any Bamarama Boxes. Stopping...');
+		if (mod.game.inventory.findAll(BAMARAMA_BOX).length === 0) {
+			mod.command.message("You do not have any Bamarama Boxes. Stopping...");
 			return;
 		}
 
 		enabled = true;
 
 		// Toggle hook in case we're still cleaning up from previous run
-		mod.game.inventory.off('update', inventoryUpdate).on('update', inventoryUpdate);
+		mod.game.inventory.off("update", inventoryUpdate).on("update", inventoryUpdate);
 
-		hook('S_SYSTEM_MESSAGE_LOOT_ITEM', 1, event => {
-			if(ITEMS.includes(event.item)) {
+		hook("S_SYSTEM_MESSAGE_LOOT_ITEM", 1, event => {
+			if (ITEMS.includes(event.item)) {
 				mod.clearTimeout(timer);
 
 				statTotal++;
-				if(event.item === ROOT_BEER) statRootBeers++;
+				if (event.item === ROOT_BEER) statRootBeers++;
 
 				openBox();
 			}
 		});
 
-		hook('C_RETURN_TO_LOBBY', 'raw', () => false); // Prevents you from being automatically logged out while AFK
+		hook("C_RETURN_TO_LOBBY", "raw", () => false); // Prevents you from being automatically logged out while AFK
 
 		openBox();
-		mod.command.message('Auto-Rootbeer started.');
+		mod.command.message("Auto-Rootbeer started.");
 	}
 
-	function hook() { hooks.push(mod.hook(...arguments)); }
-
 	function openBox() {
-		mod.send('C_USE_ITEM', 3, {
-			gameId: mod.game.me.gameId,
-			id: BAMARAMA_BOX,
-			amount: 1,
-			loc: myLoc.loc,
-			w: myLoc.w,
-			unk4: true
+		mod.send("C_USE_ITEM", 3, {
+			"gameId": mod.game.me.gameId,
+			"id": BAMARAMA_BOX,
+			"amount": 1,
+			"loc": myLoc.loc,
+			"w": myLoc.w,
+			"unk4": true
 		});
 		timer = mod.setTimeout(openBox, 5000);
 	}
@@ -74,9 +71,9 @@ module.exports = function RootBeer(mod) {
 		if (mod.settings.autoTrash) {
 			const strictTrash = mod.game.inventory.size - mod.game.inventory.items.length < 2 || !enabled;
 
-			for (let [name, id] of Object.entries(TRASH)) {
+			for (const [name, id] of Object.entries(TRASH)) {
 				if (mod.settings.autoTrashItems[name]) {
-					for (let item of mod.game.inventory.findAll(id)) {
+					for (const item of mod.game.inventory.findAll(id)) {
 						// Trashing large stacks of items is more bandwidth efficient
 						if (item.amount >= 99 || HATS.includes(id) || strictTrash) {
 							deleteItem(item);
@@ -89,37 +86,28 @@ module.exports = function RootBeer(mod) {
 		mergeHats();
 
 		if (!enabled) {
-			mod.game.inventory.off('update', inventoryUpdate); // Unhook after we've cleaned up
+			mod.game.inventory.off("update", inventoryUpdate); // Unhook after we've cleaned up
 		} else if (mod.game.inventory.findAll(BAMARAMA_BOX).length === 0) {
 			stop();
 		}
 	}
 
 	function stop() {
-		if(!enabled) return;
+		if (!enabled) return;
 
 		enabled = false;
 		mod.clearTimeout(timer);
 		timer = null;
 		unload();
 
-		mod.command.message('Auto-Rootbeer stopped.'
-			+ (!statTotal ? '' : `Unboxed ${statRootBeers}/${statTotal} (${(Math.floor(statRootBeers / statTotal * 1000) / 10) || '0'}%).`));
+		mod.command.message(`Auto-Rootbeer stopped.${
+			!statTotal ? "" : `Unboxed ${statRootBeers}/${statTotal} (${(Math.floor(statRootBeers / statTotal * 1000) / 10) || "0"}%).`}`);
 
 		statTotal = statRootBeers = 0;
 	}
 
-	function unload() {
-		if (hooks.length) {
-			for (let h of hooks) {
-				mod.unhook(h);
-			}
-			hooks = [];
-		}
-	}
-
 	function mergeHats() {
-		for (let hat of HATS.map(id => mod.game.inventory.findAll(id))) {
+		for (const hat of HATS.map(id => mod.game.inventory.findAll(id))) {
 			while (hat.length >= 2) {
 				mergeItem(hat.pop(), hat[0]);
 			}
@@ -127,20 +115,33 @@ module.exports = function RootBeer(mod) {
 	}
 
 	function mergeItem(itemFrom, itemTo) {
-		mod.send('C_MERGE_ITEM', 2, {
-			pocketFrom: itemFrom.pocket,
-			slotFrom: itemFrom.slot,
-			pocketTo: itemTo.pocket,
-			slotTo: itemTo.slot
+		mod.send("C_MERGE_ITEM", 2, {
+			"pocketFrom": itemFrom.pocket,
+			"slotFrom": itemFrom.slot,
+			"pocketTo": itemTo.pocket,
+			"slotTo": itemTo.slot
 		});
 	}
 
 	function deleteItem(item) {
-		mod.send('C_DEL_ITEM', 3, {
-			gameId: mod.game.me.gameId,
-			pocket: item.pocket,
-			slot: item.slot,
-			amount: item.amount
+		mod.send("C_DEL_ITEM", 3, {
+			"gameId": mod.game.me.gameId,
+			"pocket": item.pocket,
+			"slot": item.slot,
+			"amount": item.amount
 		});
 	}
-}
+
+	function hook() {
+		hooks.push(mod.hook(...arguments));
+	}
+
+	function unload() {
+		if (hooks.length) {
+			for (const h of hooks) {
+				mod.unhook(h);
+			}
+			hooks = [];
+		}
+	}
+};
